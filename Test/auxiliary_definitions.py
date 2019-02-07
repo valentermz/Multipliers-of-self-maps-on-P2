a9b328981e90b0577adfdb89e218076d9338dbd2
Valente Ramirez <37411799+valentermz@users.noreply.github.com>
2019-02-07 10:46:11 +0100

    Updated auxiliary-definitions

--- Test/auxiliary_definitions.py
+++ Test/auxiliary_definitions.py
@@ -1,54 +1,52 @@
 # python2
 
-"""Auxiliary definitions for TEST.ipynb"""
+"""Auxiliary definitions for the Test"""
 
 
-# Verify that the imput is valid
+# Step 1: verify that the given imput is valid
 
 def test_input(Input, td_format):
-    message = 'Input must be a list of 14 rational numbers'
-    assert isinstance(Input, list), message
-    assert len(Input) == 14, message
+    """Test wether the input given is a list of 14 rational numbers,
+    and check non-degeneracy conditions"""
+
+    error_message = 'Input must be a list of 14 rational numbers'
+
+    if isinstance(Input, list) is False:
+        print error_message
+        return False
+
+    if len(Input) != 14:
+        print error_message
+        return False
+
     for i in range(14):
-        assert isinstance(Input[i], sage.rings.integer.Integer) or isinstance(
-            Input[i], sage.rings.rational.Rational), message
-    message = 'Eigenvalues cannot be zero'
+        if (isinstance(Input[i], sage.rings.integer.Integer) or isinstance(
+                Input[i], sage.rings.rational.Rational)) is False:
+            print error_message
+            return False
+
+    error_message = 'Eigenvalues cannot be zero'
+
     if td_format is False:
         for i in range(14):
-            assert Input[i] != 0, message
+            if Input[i] == 0:
+                print error_message
+                return False
     else:
         for i in range(4):
-            assert Input[2 * i + 1] != 0, message
+            if Input[2 * i + 1] == 0:
+                print error_message
+                return False
         for i in range(8, 14):
-            assert Input[i] != 0, message
+            if Input[i] == 0:
+                print error_message
+                return False
+
     print 'Input OK'
     return True
 
 
-# Check that the previously known relations are satisfied
-
-def test_old_relations(Input, td_format):
-    u, v = build_uv(Input, td_format)
-    t, d = build_td(Input, td_format)
-    # Values that need to be checked:
-    Lefschetz0 = sum(1 / d[i] for i in range(7))
-    Lefschetz1 = sum(t[i] / d[i] for i in range(7))
-    Lefschetz2 = sum(t[i]**2 / d[i] for i in range(7))
-    Relative_Lefschetz = sum(v[i] / u[i] for i in range(4, 7))
-    Tangential_Lefschetz = sum(1 / u[i] for i in range(4, 7))
-    # Do the check
-    message = 'Relation <{}> is not satisfied'
-    assert Lefschetz0 == 1, message.format('Lefschetz0')
-    assert Lefschetz1 == 4, message.format('Lefschetz1')
-    assert Lefschetz2 == 16, message.format('Lefschetz2')
-    assert Relative_Lefschetz == 1, message.format('Relative_Lefschetz')
-    assert Tangential_Lefschetz == 1, message.format('Tangential_Lefschetz')
-    # If no error print OK
-    print 'All the previously known relations are satisfied'
-    return True
-
-
-# Build necessary variables
+# Step 2: Build necessary variables
 
 def build_uv(Input, td_format):
     u = [0 for _ in range(7)]
@@ -80,7 +78,7 @@ def build_td(Input, td_format):
     return (t, d)
 
 
-# Print the value of the variables we work with
+# Step 3: Print the value of the variables we work with
 
 def print_variables(Input, td_format):
     print 'Input:', Input, '\n'
@@ -98,7 +96,49 @@ def print_variables(Input, td_format):
         print 'v_{} ='.format(i), eval('v[{}]'.format(i)), '\n'
 
 
-# Print the final result
+# Step 4 (optional): Check that the previously known relations are satisfied
+
+def test_old_relations(Input, td_format):
+    """Test if the given input satisfies the previously known relations.
+    If this function returns False then the input is not realizable for
+    obvious reasons"""
+
+    u, v = build_uv(Input, td_format)
+    t, d = build_td(Input, td_format)
+
+    # Values that need to be checked:
+    Lefschetz0 = sum(1 / d[i] for i in range(7))
+    Lefschetz1 = sum(t[i] / d[i] for i in range(7))
+    Lefschetz2 = sum(t[i]**2 / d[i] for i in range(7))
+    Relative_Lefschetz = sum(v[i] / u[i] for i in range(4, 7))
+    Tangential_Lefschetz = sum(1 / u[i] for i in range(4, 7))
+
+    # Do the check
+    input_ok = True
+    error_message = 'Relation <{}> is not satisfied'
+    if Lefschetz0 != 1:
+        print error_message.format('Lefschetz0')
+        input_ok = False
+    if Lefschetz1 != 4:
+        print error_message.format('Lefschetz1')
+        input_ok = False
+    if Lefschetz2 != 16:
+        print error_message.format('Lefschetz2')
+        input_ok = False
+    if Relative_Lefschetz != 1:
+        print error_message.format('Relative_Lefschetz')
+        input_ok = False
+    if Tangential_Lefschetz != 1:
+        print error_message.format('Tangential_Lefschetz')
+        input_ok = False
+    # If no error print OK
+    if input_ok is True:
+        print 'All the previously known relations are satisfied'
+        return True
+    return False
+
+
+# Step 5: Print the final result
 
 def print_result(J):
     if J.dimension() != -1:
